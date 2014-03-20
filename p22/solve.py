2c18ae8fda506129da4ed7b9b768fb89818c1d2d
josephok <josephok@qq.com>
2014-03-20 10:09:35 +0800

    Update solve.py

--- p22/solve.py
+++ p22/solve.py
@@ -8,8 +8,6 @@ names.sort()
 #characters = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H': 8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
 characters = dict((item, i) for i, item in enumerate(string.ascii_uppercase, 1))
 
-characters = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H': 8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
-
 n = len(names)
 result = 0
 
