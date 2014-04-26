e5e4d0501883f1ffe2f5bd0755d35a8d99ac678b
Joseph Wu <josephok@qq.com>
2014-04-26 19:18:42 +0800

    convert tab to spaces

--- p92/solve.py
+++ p92/solve.py
@@ -1,23 +1,23 @@
 def digit_chains(n=10000000):
-	count = 0
-	for i in range(1, n):
-		start = i
-		while True:
-			if start == 1:
-				break
-			elif start == 89:
-				count += 1
-				break
-			else:
-				start = sum_of_square(start)
+    count = 0
+    for i in range(1, n):
+        start = i
+        while True:
+            if start == 1:
+                break
+            elif start == 89:
+                count += 1
+                break
+            else:
+                start = sum_of_square(start)
 
-	return count
+    return count
 
 def sum_of_square(number):
-	return sum(int(i) ** 2 for i in str(number))
+    return sum(int(i) ** 2 for i in str(number))
 
 def main():
-	print(digit_chains())
+    print(digit_chains())
 
 if __name__ == "__main__":
-	main()
+    main()
--- p92/test.py
+++ p92/test.py
@@ -2,12 +2,12 @@ import unittest
 from solve import sum_of_square
 
 class MyTest(unittest.TestCase):
-	def test_sum_if_square(self):
-		self.assertEqual(1, sum_of_square(1))
-		self.assertEqual(4, sum_of_square(2))
-		self.assertEqual(9, sum_of_square(3))
-		self.assertEqual(32, sum_of_square(44))
-		self.assertEqual(89, sum_of_square(85))
+    def test_sum_if_square(self):
+        self.assertEqual(1, sum_of_square(1))
+        self.assertEqual(4, sum_of_square(2))
+        self.assertEqual(9, sum_of_square(3))
+        self.assertEqual(32, sum_of_square(44))
+        self.assertEqual(89, sum_of_square(85))
 
 if __name__ == '__main__':
     unittest.main()
